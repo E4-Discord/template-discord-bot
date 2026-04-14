ae08215f8b5f3c32b5c2059c73985905cf899239
이정훈 <eu4ng97@gmail.com>
2026-04-14 21:47:37 +0900

    feat: 주사위 명령어 추가

---
+++ src/Cogs/dice.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import random
+
+class Dice(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name='주사위', description='주사위를 굴립니다.')
+    async def dice(self, interaction: discord.Interaction):
+        num = random.randrange(1, 7)
+        await interaction.response.send_message(f'{num}이 나왔습니다!')
+
+async def setup(bot):
+    await bot.add_cog(Dice(bot))
