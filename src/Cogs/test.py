7f6bc86454a08f2786c0601263a50133c6655e08
이정훈 <eu4ng97@gmail.com>
2026-04-15 01:34:16 +0900

    feat: 현재 봇 버전 출력 기능 추가

--- src/Cogs/test.py
+++ src/Cogs/test.py
@@ -1,11 +1,19 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
-import random
+import os
 
 class Test(commands.Cog):
     def __init__(self, bot):
-        self.bot = bot  
+        self.bot = bot
+
+    @app_commands.command(name='버전', description='현재 봇 버전을 확인합니다.')
+    async def version(self, interaction: discord.Interaction):
+        await interaction.response.defer(thinking=True)
+        
+        version = os.getenv('version')
+
+        await interaction.followup.send(f'{version}')
 
     @app_commands.command(name='테스트', description='테스트 전용 명령어입니다.')
     async def test(self, interaction: discord.Interaction):
