4f164a964c8cf7833b743229b285bc28361a3ea8
이정훈 <eu4ng97@gmail.com>
2026-04-14 23:45:54 +0900

    fix: 주사위 명령어를 defer 방식으로 변경

--- src/Cogs/dice.py
+++ src/Cogs/dice.py
@@ -9,8 +9,9 @@ class Dice(commands.Cog):
 
     @app_commands.command(name='주사위', description='주사위를 굴립니다.')
     async def dice(self, interaction: discord.Interaction):
+        await interaction.response.defer(thinking=True)
         num = random.randrange(1, 7)
-        await interaction.response.send_message(f'{num}')
+        await interaction.followup.send(f'{num}')
 
 async def setup(bot):
     await bot.add_cog(Dice(bot))
