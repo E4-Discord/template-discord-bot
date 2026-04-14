d42292b8fd1d9adf7c75607c5167d2b4a1ddfd34
이정훈 <eu4ng97@gmail.com>
2026-04-14 23:08:10 +0900

    fix: 주사위 명령어 수정

--- src/Cogs/dice.py
+++ src/Cogs/dice.py
@@ -10,7 +10,7 @@ class Dice(commands.Cog):
     @app_commands.command(name='주사위', description='주사위를 굴립니다.')
     async def dice(self, interaction: discord.Interaction):
         num = random.randrange(1, 7)
-        await interaction.response.send_message(f'{num}이 나왔습니다!')
+        await interaction.response.send_message(f'{num}')
 
 async def setup(bot):
     await bot.add_cog(Dice(bot))
